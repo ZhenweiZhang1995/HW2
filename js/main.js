be85056130c92d9243ebcb949a83d11abe3f2d2f
ZhenweiZhang1995 <apple@dhcp-v880-8-00138.1Xwireless.unc.edu>
2017-01-27 09:18:19 -0500

    v1.0
    “done” button doesn’t work

---
+++ js/main.js
@@ -0,0 +1,66 @@
+// hint -- remove a particular element from an array: http://stackoverflow.com/questions/5767325/how-to-remove-a-particular-element-from-an-array-in-javascript
+// hint -- add an item to an array: http://stackoverflow.com/questions/351409/how-to-append-something-to-an-array
+// hint -- you may need an "editingItem" variable in your data object
+
+new Vue({
+  el: '#app',
+  methods: {
+    cancel() {
+        this.itemTitle = '';
+        this.itemContent = '';
+      },
+      addItem() {
+        this.items.push({
+          itemTitle: this.newItemTitle,
+          itemContent: this.newItemContent,
+          favorite: this.newFavorite
+        })
+        this.newItemTitle = '';
+        this.newItemContent = '';
+        this.newFavorite = false;
+      },
+      editBtn() {
+        alert("Still under constrection");
+      },
+      done() {
+        this.finished = true;
+        // items.$remove(this.itemTitle);
+        // print(items[0]);
+        // print($vm0.items);
+      }
+  },
+
+  data: {
+    newItemTitle: '',
+    newItemContent: '',
+    finished: false,
+    newFavorite: false,
+    editingItem: null,
+    items: [{
+      itemTitle: "Watch a movie",
+      itemContent: "Zootopia on Sunday 6pm ",
+      favorite: true
+    }, {
+      itemTitle: "Finish STOR415 Homework",
+      itemContent: "Finish your homework before you do anything else!!! ",
+      favorite: false
+    }, {
+      itemTitle: "Play Overwatch",
+      itemContent: "OK Let's do it",
+      favorite: false
+    }]
+  }
+  // methods: {
+  //   cancel() {
+  //     this.itemTitle = '';
+  //     this.itemContent = '';
+  //   }
+  //
+  //   editBtn() {
+  //     alert("Still under constrection");
+  //   }
+  //   done() {
+  //     alert("Still under constrection");
+  //   }
+  // }
+});
